7825e0ae82bd7975e15be293ae78eb15090ce464
Michael Kelly <m@michaelkelly.org>
2024-06-04 22:23:58 -0700

    Tweak site generation settings

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hugo --minify --cleanDestinationDir -d ./public || exit 1
+hugo --gc --minify --cleanDestinationDir -d ./public || exit 1
 ./scripts/git-revision-stamp.sh > ./public/revision.txt || exit 1
